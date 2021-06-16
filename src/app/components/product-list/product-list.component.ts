13b93fb55b35dd160db5d8462606787f032b3033
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-16 11:06:17 +0200

    List of projects from the backend displayed in a table

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  private listProducts(): void {
+    // .getProductList is async method, hence subscribe waits until all info is read.
+    this.productService.getProductList().subscribe(
+      data => {this.products = data;}
+    )
+  }
+
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from '../common/product';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl: string = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.log(error);
+      return of(result as T);
+    };
+  }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products),
+      catchError(this.handleError<Product[]>('getProducts', []))
+    );
+  }
+}
+
+  interface GetResponse {
+    _embedded: {
+      products: Product[];
+    }
+  }

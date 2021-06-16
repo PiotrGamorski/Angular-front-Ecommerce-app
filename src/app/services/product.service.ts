33e29bb484843e347d3b204e412a975f1687087b
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-16 14:25:57 +0200

    New icons added and product-list-table exchanged with product-list-grid

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { catchError, map } from 'rxjs/operators';
 })
 export class ProductService {
 
-  private baseUrl: string = 'http://localhost:8080/api/products';
+  private baseUrl: string = 'http://localhost:8080/api/products?size=100';
 
   constructor(private httpClient: HttpClient) { }
 

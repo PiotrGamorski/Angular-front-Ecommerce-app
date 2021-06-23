3086c1e1e6fe387158c3b35cb7792f9305feb47d
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-23 11:52:21 +0200

    ProductListComponent updated with pagination

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -6,84 +6,89 @@ import { catchError, map } from 'rxjs/operators';
 import { ProductCategory } from '../common/product-category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
   private baseUrl: string = 'http://localhost:8080/api/products';
   private categoryUrl: string = 'http://localhost:8080/api/product-category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  private handleError<T>(operation = 'operation', result?: T){
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.log(error);
       return of(result as T);
     };
-  };
+  }
 
-  private getProducts(searchUrl: string, getter: string): Observable<Product[]> {
-
+  private getProducts(
+    searchUrl: string,
+    getter: string
+  ): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products),
+      map((response) => response._embedded.products),
       catchError(this.handleError<Product[]>(getter, []))
     );
-  };
+  }
 
   // check in Postman 'http://localhost:8080/api/products/1" to see the JSON. No need to format the reponse!
-  getProduct(theProductId: number): Observable<Product>{
-
+  getProduct(theProductId: number): Observable<Product> {
     const productUrl: string = `${this.baseUrl}/${theProductId}`;
 
     return this.httpClient.get<Product>(productUrl);
   }
 
-  getProductListPaginate(thePage: number, thePageSize: number, theCategoryId: number): Observable<GetResponseProducts> {
-
-    const searchUrl: string = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` 
-    + `&page=${thePage}&size=${thePageSize}`;
+  getProductListPaginate(
+    thePage: number,
+    thePageSize: number,
+    theCategoryId: number
+  ): Observable<GetResponseProducts> {
+    const searchUrl: string =
+      `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` +
+      `&page=${thePage}&size=${thePageSize}`;
 
     return this.httpClient.get<GetResponseProducts>(searchUrl);
-  };
+  }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-
-    const searchUrl: string = this.baseUrl + "/search/findByCategoryId?id=" + theCategoryId;
+    const searchUrl: string =
+      this.baseUrl + '/search/findByCategoryId?id=' + theCategoryId;
 
     return this.getProducts(searchUrl, 'getProducts');
-  };
+  }
 
   searchProducts(theKeyword: string | undefined): Observable<Product[]> {
-
     const searchUrl: string = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl, 'getProducts');
-  };
-
-  getProductCategories(): Observable<ProductCategory[]>{
-
-    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.productCategory),
-      catchError(this.handleError<ProductCategory[]>('getProductCategories', []))
-    );
-  };
-
-}; // END OF CLASS
-
-  interface GetResponseProducts {
-    _embedded: {
-      products: Product[];
-    },
-    page: {
-      size: number,
-      totalElements: number,
-      totalPages: number,
-      number: number,
-    }
-  };
-
-  interface GetResponseProductCategory {
-    _embedded: {
-      productCategory: ProductCategory[];
-    }
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(
+        map((response) => response._embedded.productCategory),
+        catchError(
+          this.handleError<ProductCategory[]>('getProductCategories', [])
+        )
+      );
+  }
+} // END OF CLASS
+
+export interface GetResponseProducts {
+  _embedded: {
+    products: Product[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
+  };
+}
+
+export interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  };
+}

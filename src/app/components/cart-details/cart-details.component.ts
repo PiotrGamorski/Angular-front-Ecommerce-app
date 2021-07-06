c231f1f8998fc65cd00eaa8674cc008ecb45c42b
G02\GamorskiP <piotr.gamorski@ts.fujitsu.com>
2021-07-06 13:07:07 +0200

    Increment and decrement logic implemented. Removing with a button also added.

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -32,4 +32,32 @@ export class CartDetailsComponent implements OnInit {
     // compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(theCartItem: CartItem): void {
+    this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity(theCartItem: CartItem): void {
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.cartService.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem): void {
+    const itemIndex: number = this.cartItems.findIndex(
+      (tempCartItem) => tempCartItem.id === theCartItem.id
+    );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      // at position: itemIndex removes 1 element
+      this.cartItems.splice(itemIndex, 1);
+
+      this.cartService.computeCartTotals();
+    }
+  }
 }

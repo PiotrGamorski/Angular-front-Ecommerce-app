58dc16202da27cd6ae7565475cedac1f999fb8e1
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-07-08 10:46:18 +0200

    New FormGroups added to checkout.ts component

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -12,13 +12,54 @@ export class CheckoutComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.checkoutFormGroup = this.formBuilder.group({
-      customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
-      }),
+    const customer: FormGroup = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
     });
+
+    const shippingAddress: FormGroup = this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      country: [''],
+      zipCode: [''],
+    });
+
+    const billingAddress: FormGroup = this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      country: [''],
+      zipCode: [''],
+    });
+
+    const creditCard: FormGroup = this.formBuilder.group({
+      cardType: [''],
+      nameOnCard: [''],
+      cardNumber: [''],
+      securityCode: [''],
+      expirationMonth: [''],
+      expirationYear: [''],
+    });
+
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: customer,
+      shippingAddress: shippingAddress,
+      billingAddress: billingAddress,
+      creditCard: creditCard,
+    });
+    // the parent form group is 'checkoutFormGroup' and 'customer' is nested form group
+
+    // console logs every value change
+    this.checkoutFormGroup
+      .get('customer')
+      ?.valueChanges.subscribe((data) => console.log(data));
+  }
+
+  onSubmit() {
+    console.log('Handling the submit button');
+    console.log(this.checkoutFormGroup.get('customer')?.value);
   }
 
   // END OF CLASS

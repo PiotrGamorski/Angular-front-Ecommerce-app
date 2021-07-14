cbfe95e6b2ada96e9fb8fb7ae5c482f7860226e7
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-07-14 08:40:15 +0200

    Validation for customer in checkout component implemented

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
@@ -32,6 +37,18 @@ export class CheckoutComponent implements OnInit {
     private luv2ShopFormService: Luv2ShopFormService
   ) {}
 
+  get firstName() {
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName() {
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email() {
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
   ngOnInit(): void {
     this.listCheckoutDetails();
     this.populateCreditCardMonths();
@@ -40,9 +57,18 @@ export class CheckoutComponent implements OnInit {
     this.handleListOfAllStates();
 
     const customer: FormGroup = this.formBuilder.group({
-      firstName: [''],
-      lastName: [''],
-      email: [''],
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+      ]),
     });
 
     const shippingAddress: FormGroup = this.formBuilder.group({
@@ -76,7 +102,6 @@ export class CheckoutComponent implements OnInit {
       billingAddress: billingAddress,
       creditCard: creditCard,
     });
-    // the parent form group is 'checkoutFormGroup' and 'customer' is nested form group
 
     // console logs every value change
     this.checkoutFormGroup
@@ -132,7 +157,10 @@ export class CheckoutComponent implements OnInit {
 
   onSubmit() {
     console.log('Handling the submit button');
-    console.log(this.checkoutFormGroup.get('customer')?.value);
+
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
   }
 
   copyShippingAddressToBilingAddress(theEvent: Event): void {

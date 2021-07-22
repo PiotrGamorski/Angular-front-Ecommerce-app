09c0f35642c3e1f1019cb95ed77a24838cdb5edb
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-07-22 13:02:22 +0200

    Rendering sign in widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   // when path is hit (it matches), then a new instance of ProductListComponent is created.
@@ -40,6 +41,7 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),

fb87ca56435edba8d0e7ca4448224eba38703f3c
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-07-22 12:08:58 +0200

    Okta dependencies installed

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa1b0mufwcQp2fnq5d7',
+    issuer: 'https://dev-26917249.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
